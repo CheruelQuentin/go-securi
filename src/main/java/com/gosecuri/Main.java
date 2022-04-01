d767b7dfabc04ec9492fd427f7fecf577bcdf601
Lucas Belin <lucasbelinpro@gmail.com>
2022-04-01 09:08:00 +0200

    refactor folder structure

--- src/main/java/com/gosecuri/Main.java
+++ src/main/java/com/gosecuri/Main.java
@@ -9,18 +9,18 @@ import java.util.List;
 import com.gosecuri.htmlgeneration.AgentPageGenerator;
 import com.gosecuri.htmlgeneration.IndexPageGenerator;
 
-import static com.gosecuri.utils.PathUtils.AGENT_TEXT_FILES_PATH;
+import static com.gosecuri.utils.PathUtils.AGENT_TEXT_FILES_FOLDER_PATH;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String dirPath = System.getProperty("user.dir");
-        File f = new File(AGENT_TEXT_FILES_PATH);
+        File f = new File(AGENT_TEXT_FILES_FOLDER_PATH);
 
         String[] files = f.list();
         if(files != null && files.length > 0) {
             for (String file : files) {
-                List<String> agentData = Files.readAllLines(Path.of(AGENT_TEXT_FILES_PATH + file));
+                List<String> agentData = Files.readAllLines(Path.of(AGENT_TEXT_FILES_FOLDER_PATH + file));
                 AgentPageGenerator apg = new AgentPageGenerator(agentData);
                 apg.generateHTML();
             }
